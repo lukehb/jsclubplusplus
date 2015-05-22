b715143fce95437514c8b0afd810eb82f47ca758
unknown <jc222776@SE01-015-W14.ad.jcu.edu.au>
2015-05-22 15:57:21 +1000

    scrothced earth

--- Week2/StringProcessor.js
+++ Week2/StringProcessor.js
@@ -1,41 +1,5 @@
 function StringProcessor() {}
 
-//Returns the middle character of a string if the string length is odd,
-//otherwise returns the middle two characters of a string if the length is even
-StringProcessor.prototype.middleChar = function(str)
-{
-	return "think again";
-}
 
-//Returns the first word in a string that has a length of at least seven letters
-StringProcessor.prototype.firstWordWithAtLeastSevenLetters = function(str)
-{
-	return "Seven";
-}
-
-//Returns all of the vowels in a string
-StringProcessor.prototype.onlyVowels = function(str) {
-	return "aeiou";
-}
-
-//Converts all vowels in a string to uppercase
-StringProcessor.prototype.uppercaseVowels = function(str) {
-	return null;
-}
-
-//Replaces every instance of the letter J with an exclamation mark
-StringProcessor.prototype.replaceJWithExclamation = function(str) {
-	return null;
-}
-
-//Doubles every instance of a consonant
-StringProcessor.prototype.doubleConsonants = function(str) {
-	return null;
-}
-
-//Inserts the string "and" in the middle of every two-letter word
-StringProcessor.prototype.andifyTwoLetterWords = function(str) {
-	return null;
-}
 
 module.exports = StringProcessor;
