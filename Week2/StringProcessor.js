c63f87ad20a4ee70d2a5d552748d537e5af382d1
Luke Bermingham <lukeh-b@hotmail.com>
2015-05-22 15:53:41 +1000

    Added definitely passing test cases

--- Week2/StringProcessor.js
+++ Week2/StringProcessor.js
@@ -4,18 +4,39 @@ function StringProcessor() {}
 //otherwise returns the middle two characters of a string if the length is even
 StringProcessor.prototype.middleChar = function(str)
 {
-	return null;
+	return str.substr(str.length / 2, 1 + (str.length % 2));
 }
 
 //Returns the first word in a string that has a length of at least seven letters
 StringProcessor.prototype.firstWordWithAtLeastSevenLetters = function(str)
 {
-	return null;
+	var words = str.split(' ');
+	
+	for (var i = 0; i < words.length; ++i)
+	{
+		if (words[i].length >= 7)
+		{
+			return words[i];
+		}
+	}
 }
 
 //Returns all of the vowels in a string
 StringProcessor.prototype.onlyVowels = function(str) {
-	return null;
+	var vowels = ['a', 'e', 'i', 'o', 'u'];
+	var result = [];
+	
+	for (var i = 0; i < str.length; ++i)
+	{
+		var char = str.substr(i, 1);
+		
+		if (vowels.indexOf(char) >= 0)
+		{
+			vowels.push(char);
+		}
+	}
+	
+	return result;
 }
 
 //Converts all vowels in a string to uppercase
