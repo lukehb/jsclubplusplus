db208cba88b1b06980634c542cf5e5ea2eb8e638
Adam Rehn <adam.rehn@my.jcu.edu.au>
2015-05-22 15:06:11 +1000

    Added the StringProcessor answer stubs.

--- Week2/StringProcessor.js
+++ Week2/StringProcessor.js
@@ -4,44 +4,38 @@ function StringProcessor() {}
 //otherwise returns the middle two characters of a string if the length is even
 StringProcessor.prototype.middleChar = function(str)
 {
-	if (str.length % 2 == 0) {
-		return str.substr( Math.floor(str.length / 2) - 1, 2 );
-	}
-	else {
-		return str.substr( Math.floor(str.length / 2), 1 );
-	}
+	return null;
 }
 
 //Returns the first word in a string that has a length of at least seven letters
 StringProcessor.prototype.firstWordWithAtLeastSevenLetters = function(str)
 {
-	var matches = str.match(/([a-z|A-Z]{7,})/);
-	return matches[1];
+	return null;
 }
 
 //Returns all of the vowels in a string
 StringProcessor.prototype.onlyVowels = function(str) {
-	return str.match(/[aeiouAEIOU]/g).join('');
+	return null;
 }
 
 //Converts all vowels in a string to uppercase
 StringProcessor.prototype.uppercaseVowels = function(str) {
-	return str.replace(/[aeiouAEIOU]/g, function(s) { return s.toUpperCase(); });
+	return null;
 }
 
 //Replaces every instance of the letter J with an exclamation mark
 StringProcessor.prototype.replaceJWithExclamation = function(str) {
-	return str.replace('j', '!').replace('J', '!');
+	return null;
 }
 
 //Doubles every instance of a consonant
 StringProcessor.prototype.doubleConsonants = function(str) {
-	return str.replace(/[^aeiouAEIOU]{1}/g, '$&$&');
+	return null;
 }
 
 //Inserts the string "and" in the middle of every two-letter word
 StringProcessor.prototype.andifyTwoLetterWords = function(str) {
-	return str.replace(/\b([a-z|A-Z])([a-z|A-Z])\b/g, "$1and$2");
+	return null;
 }
 
 module.exports = StringProcessor;
